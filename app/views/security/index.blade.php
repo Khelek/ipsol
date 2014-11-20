e22dba63c77662ad84fa1079cc4e79e6f75c0fda
Anastasia <rocksellx3@gmail.com>
2014-11-20 10:20:34 +0400

    add security html/css

--- app/views/security/index.blade.php
+++ app/views/security/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.default')
 
 @section('title')
-    {{{ $title }}}
 @stop
 
 @section('keywords')Posts administration @stop
@@ -10,9 +9,38 @@
 
 
 @section('content')
+	<div class="ban clear-fix">
+		<div class="ban_gray">
+			<div class="col2">
+				<h1><span class="ban_text">Советы по безопасности</span></h1>
+			</div>
+		</div>
+		<div class="ban_orange">
+			<div class="col2">
+				<div class="triangle"></div>
+			</div>
+		</div>
+	</div>
+	<div class="content clear-fix">
+	    <div class="main">
+	    	<div class="article clear-fix">
+	    		<img src="{{	asset('assets/images/article_img_small.jpg') }}" alt="">
+	    		<div class="article_container">
+		    		<h2><a class="article_header" href="">В Французуком городе</a></h2>
+		    		<p class="description"> Много много текста всягоко разного, увлекательного
+		    		 и интересного, может быть даже полезного </p>
+					<p class="date">30/09/2014</p>
+					    Тэги:
+                    <a href="" class="tag">красивый тег</a>
+					<div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="none" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,gplus"></div>
+	    		</div>
+	    	</div>
+	    </div>
+	</div>
 @stop
-
-
+	
 @section('styles')
 	  <link rel="stylesheet" href="{{	asset('assets/css/blog.css') }}">
+	  <link rel="stylesheet" href="{{	asset('assets/css/theme.css') }}">
+	  <link rel="stylesheet" href="{{	asset('assets/css/security.css') }}">
 @stop
