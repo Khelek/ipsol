52cd32cbc7d754780abf00e385a6eafb33f4e62f
Dmitry Davydov <haudvd@gmail.com>
2014-11-20 12:02:50 +0300

    add support base

--- app/routes.php
+++ app/routes.php
@@ -29,3 +29,9 @@ Route::resource('blogs', 'BlogsController', ['except' => ['show']]);
 
 Route::get('security/{slug}', ['as' => 'security.show', 'uses' => 'SecurityController@show'])->where('slug', '[A-Za-z0-9\-]+');
 Route::resource('security', 'SecurityController', ['except' => ['show']]);
+
+
+Route::get('support/ask', ['as' => 'support.ask', 'uses' => 'SupportController@ask']);
+Route::get('support/sip', ['as' => 'support.sip', 'uses' => 'SupportController@sip']);
+Route::get('support/pod', ['as' => 'support.pod', 'uses' => 'SupportController@pod']);
+Route::get('support/ask/{slug}', ['as' => 'support.ask.show', 'uses' => 'SupportController@ask_show'])->where('slug', '[A-Za-z0-9\-]+');
