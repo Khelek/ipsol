ad9a55e417bd142a5ac3b02cddadf20a34753099
Anastasia <rocksellx3@gmail.com>
2014-11-20 13:39:43 +0400

    add pod css/html

--- app/views/support/pod.blade.php
+++ app/views/support/pod.blade.php
@@ -27,14 +27,89 @@
 			</div>
 		</div>
 	</div>
-	<div class="content clear-fix">
-	    
+	<div class="address_container content clear-fix">
+	    <h1 class="title">Адреса сервисных центров</h1>
+	    <div class="clear-fix">
+		    <div class="col2">
+		    	<div class="address_block clear-fix">
+					<h2>Веб камера GRT-45JkL</h2>
+					<p>Адреса:</p>
+					<p class="address ">
+						Москва, ул. Колотушкина, 1<br>
+						Москва, ул. Можайского, 14<br>
+						Москва, ул. Крутые Ключище, 5<br>
+						Москва, ул. Матросова, 10 <br>
+						Москва, ул. Рейкеса, 11 <br>
+					</p>
+		    	</div>
+		    	<div class="address_block clear-fix">
+					<h2>Веб камера GRT-45JkL</h2>
+					<p>Адреса:</p>
+					<p class="address ">
+						Москва, ул. Колотушкина, 1<br>
+						Москва, ул. Можайского, 14<br>
+						Москва, ул. Крутые Ключище, 5<br>
+						Москва, ул. Матросова, 10 <br>
+						Москва, ул. Рейкеса, 11 <br>
+					</p>
+		    	</div>
+		    	<div class="address_block clear-fix">
+					<h2>Веб камера GRT-45JkL</h2>
+					<p>Адреса:</p>
+					<p class="address ">
+						Москва, ул. Колотушкина, 1<br>
+						Москва, ул. Можайского, 14<br>
+						Москва, ул. Крутые Ключище, 5<br>
+						Москва, ул. Матросова, 10 <br>
+						Москва, ул. Рейкеса, 11 <br>
+					</p>
+		    	</div>
+		    </div>
+		    <div class="col2">
+				<div class="address_block clear-fix">
+					<h2>Веб камера GRT-45JkL</h2>
+					<p>Адреса:</p>
+					<p class="address ">
+						Москва, ул. Колотушкина, 1<br>
+						Москва, ул. Можайского, 14<br>
+						Москва, ул. Крутые Ключище, 5<br>
+						Москва, ул. Матросова, 10 <br>
+						Москва, ул. Рейкеса, 11 <br>
+					</p>
+		    	</div>
+		    	<div class="address_block clear-fix">
+					<h2>Веб камера GRT-45JkL</h2>
+					<p>Адреса:</p>
+					<p class="address ">
+						Москва, ул. Колотушкина, 1<br>
+						Москва, ул. Можайского, 14<br>
+						Москва, ул. Крутые Ключище, 5<br>
+						Москва, ул. Матросова, 10 <br>
+						Москва, ул. Рейкеса, 11 <br>
+					</p>
+		    	</div>
+		    </div>
+	   	</div>
+	    <button class="button button_active">Показать всё</button>
 	</div>
+	<div class="content clear-fix">
+	    <h1 class="title">Нужна помощь?</h1>
+	    <p>Просто заполните заявку и мы свяжемся с Вами в течении дня и мгновенно <br>
+проконсультируем!</p>
+		<div class="ask_form">
+			<div class="clear-fix">
+				<input type="text" placeholder="Ваше имя" class="input">
+				<input type="text" placeholder="Ваш телефон" class="input">
+				<input type="text" placeholder="Ваш e-mail" class="input">
+			</div>
+			<button class="button button_active">Оставить заявку</button>
+		</div>
+	</div>
+
 
 @stop
 
 
 @section('styles')
-	  <link rel="stylesheet" href="{{	asset('assets/css/security_open.css') }}">
+	  <link rel="stylesheet" href="{{	asset('assets/css/pod.css') }}">
 @stop
- 
