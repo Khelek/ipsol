e758b240067bd14132c7087f28847b679396aac8
Anastasia Grishina <rocksellx3@gmail.com>
2014-11-26 00:56:10 +0300

    add file

--- app/views/news/index.blade.php
+++ app/views/news/index.blade.php
@@ -23,6 +23,23 @@
 		</div>
 	</div>
 	<div class="content clear-fix">
+	<div class="works_images clear-fix">
+		<img class="work_img_big" src="{{	asset('assets/images/article_img_big.jpg') }}" alt="">
+		<img class="work_img_small" src="{{	asset('assets/images/article_img_small.jpg') }}" alt="">
+		<img class="work_img_small" src="{{	asset('assets/images/article_img_small.jpg') }}" alt="">
+	</div>
+	 <div class="main">
+          @foreach ($tips as $tip)
+	    	      <div class="article clear-fix">
+	    		        <img src="{{ $tip->preview->url('medium') }}" alt="">
+	    		        <div class="article_container">
+		    		          <h2><a class="article_header" href="{{	URL::route('security.show', $tip->slug) }}">{{ $tip->title }}</a></h2>
+		    		          <p class="description"> {{ Str::limit($tip->content, 100) }} </p>
+					            <p class="date">{{ $tip->created_at() }}</p>
+	    		        </div>
+	    	      </div>
+        @endforeach
+	    </div>
 	</div>
 
 <?php $paginator = $news; ?>
@@ -31,5 +48,5 @@
 
 
 @section('styles')
-	  <link rel="stylesheet" href="{{	asset('assets/css/blog.css') }}">
+	  <link rel="stylesheet" href="{{	asset('assets/css/news.css') }}">
 @stop
