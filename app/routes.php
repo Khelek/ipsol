fc5556a9081bcca822a43008b1ed0936516f8095
Dmitry Davydov <haudvd@gmail.com>
2014-12-31 09:51:20 +0300

    change slug symbols

--- app/routes.php
+++ app/routes.php
@@ -77,9 +77,9 @@ Route::get('login', 'UserController@getLogin');
 Route::get('logout', 'UserController@postLogin');
 
 Route::get('blogs/{slug}', ['as' => 'blogs.show',
-                            'uses' => 'BlogsController@show'])->where('slug', '[A-Za-z0-9\-]+');
+                            'uses' => 'BlogsController@show'])->where('slug', '[A-Za-z0-9\-_]+');
 Route::get('blogs/rubric/{slug}', ['as' => 'blogs.index.rubric',
-                            'uses' => 'BlogsController@index'])->where('slug', '[A-Za-z0-9\-]+');
+                            'uses' => 'BlogsController@index'])->where('slug', '[A-Za-z0-9\-_]+');
 Route::resource('blogs', 'BlogsController', ['except' => ['show']]);
 
 
@@ -88,23 +88,23 @@ Route::resource('requests', 'RequestsController', ['only' => ['store']]);
 
 
 Route::get('security/{slug}', ['as' => 'security.show',
-                               'uses' => 'SecurityController@show'])->where('slug', '[A-Za-z0-9\-]+');
+                               'uses' => 'SecurityController@show'])->where('slug', '[A-Za-z0-9\-_]+');
 Route::resource('security', 'SecurityController', ['except' => ['show']]);
 
 Route::get('news/{slug}', ['as' => 'news.show',
-                           'uses' => 'NewsController@show'])->where('slug', '[A-Za-z0-9\-]+');
+                           'uses' => 'NewsController@show'])->where('slug', '[A-Za-z0-9\-_]+');
 Route::resource('news', 'NewsController', ['except' => ['show']]);
 
 
 Route::get('works/{slug}', ['as' => 'works.show',
-                            'uses' => 'WorksController@show'])->where('slug', '[A-Za-z0-9\-]+');
+                            'uses' => 'WorksController@show'])->where('slug', '[A-Za-z0-9\-_]+');
 Route::get('works/categories/{slug}', ['as' => 'works.index.category',
-                            'uses' => 'WorksController@index'])->where('slug', '[A-Za-z0-9\-]+');
+                            'uses' => 'WorksController@index'])->where('slug', '[A-Za-z0-9\-_]+');
 Route::resource('works', 'WorksController', ['except' => ['show']]);
 
 
 Route::get('support/ask/{slug}', ['as' => 'support.ask.show',
-                                  'uses' => 'SupportController@ask_show'])->where('slug', '[A-Za-z0-9\-]+');
+                                  'uses' => 'SupportController@ask_show'])->where('slug', '[A-Za-z0-9\-_]+');
 Route::get('support/ask', ['as' => 'support.ask', 'uses' => 'SupportController@ask']);
 Route::get('support/sip', ['as' => 'support.sip', 'uses' => 'SupportController@sip']);
 Route::get('support/pod', ['as' => 'support.pod', 'uses' => 'SupportController@pod']);
