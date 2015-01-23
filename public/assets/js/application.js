13a8d351cadfbb8b21b3add117c2ce280e92935a
Anastasia Grishina <rocksellx3@gmail.com>
2015-01-23 16:10:59 +0300

    html css fix

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -2133,12 +2133,12 @@ $(document).ready(function() {
 		 	function(){ 
 				$('#modal_form') 
 					.css('display', 'block')
-					.animate({opacity: 1, top: '50%'}, 200);
+					.animate({opacity: 1}, 200);
 		});
 	});
 	$('#modal_close, #overlay').click( function(){ 
 		$('#modal_form')
-			.animate({opacity: 0, top: '45%'}, 200,  
+			.animate({opacity: 0}, 200,  
 				function(){ 
 					$(this).css('display', 'none'); 
 					$('#overlay').fadeOut(400);
